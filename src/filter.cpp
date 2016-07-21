4066c63c1ba8eddb7e7ce8b694a9527f14b7d9e5
Evan McNeil <evanmcneil1999@gmail.com>
2016-07-21 05:19:02 -0700

    Fixed filter (uses cin instead of whatever varun had)

--- src/filter.cpp
+++ src/filter.cpp
@@ -1,24 +1,29 @@
-#include<iostream>
-#include<fstream>
-#include<unordered_set>
-#include<string>
+#include <iostream>
+//#include<fstream>
+#include <unordered_set>
+#include <string>
 
 int main(){
-	std::ifstream wall ("wall", std::ifstream::in);
+	//std::ifstream wall ("wall", std::ifstream::in);
 	std::unordered_set<std::string> values;
-	std::string next_line;
-	while( std::getline(wall, next_line) ) {
+	//std::string next_line;
+	/*
+	while( std::getline(wall, next_line) ) 
+	{
 		values.insert(next_line);
 		
 	}
+	*/
+	std::string line;
+	while (std::getline(std::cin, line))
+	{
+		values.insert(line);
+	}
 	
-	for(auto i = values.begin(); i != values.end(); i++){
+	for(auto i = values.begin(); i != values.end(); i++)
+	{
 		std::cout << *i << std::endl;
 	}
 	return 0;
 
 }
-			
-					
-
-	
